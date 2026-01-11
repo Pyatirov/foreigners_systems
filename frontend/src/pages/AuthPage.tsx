2d97d007ecf9732564f7defd5146305f7cebca50
Denis Pyatirov <denis.pyatirov@gmail.com>
2026-01-11 21:06:47 +0300

    Переработка страницы авторизации

--- frontend/src/pages/AuthPage.tsx
+++ frontend/src/pages/AuthPage.tsx
@@ -1,13 +1,39 @@
-import { Box, Paper, Typography, TextField, Button, MenuItem, Select, FormControl, InputLabel } from "@mui/material";
+import { Box, Paper, Typography, TextField, Button } from "@mui/material";
 import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { loginRequest } from "../api/auth.api";
+import { useAuth } from "../context/AuthContext";
 
 export const AuthPage = () => {
-  const [username, setUsername] = useState("");
-  const [password, setPassword] = useState("");
-  const [role, setRole] = useState("specialist");
 
-  const handleLogin = () => {
-    console.log("Логин:", username, "Пароль:", password, "Роль:", role);
+  const navigate = useNavigate();
+  const { login } = useAuth();
+
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState("")
+
+  const handleLogin = async () => {
+    try {
+      console.log("Login started", email, password)
+
+      if (!email || !password) {
+        setError("Введите логин и пароль");
+        return
+      }
+
+      const accessToken = await loginRequest( { email, password })
+
+      console.log("Login success, token:", accessToken)
+
+      login(accessToken);
+
+      navigate("/", { replace: true })
+      console.log('Logged in');
+    } catch(err: any) {
+      console.error("Login error", err)
+      setError("Неверный логин или пароль");
+    }
   };
 
   return (
@@ -36,23 +62,10 @@ export const AuthPage = () => {
           Вход
         </Typography>
 
-        <FormControl fullWidth>
-          <InputLabel id="role-label">Тип пользователя</InputLabel>
-          <Select
-            labelId="role-label"
-            value={role}
-            label="Тип пользователя"
-            onChange={(e) => setRole(e.target.value)}
-          >
-            <MenuItem value="specialist">Специалист</MenuItem>
-            <MenuItem value="admin">Администратор</MenuItem>
-          </Select>
-        </FormControl>
-
         <TextField
           label="Логин"
-          value={username}
-          onChange={(e) => setUsername(e.target.value)}
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
           fullWidth
         />
         <TextField
@@ -63,6 +76,12 @@ export const AuthPage = () => {
           fullWidth
         />
 
+        {error && (
+          <Typography color="error" fontSize={14}>
+            {error}
+          </Typography>
+        )}
+
         <Button variant="contained" onClick={handleLogin} fullWidth>
           Войти
         </Button>
@@ -70,3 +89,77 @@ export const AuthPage = () => {
     </Box>
   );
 };
+
+// import { useState } from "react";
+// import { TextField, Button, Box, Typography } from "@mui/material";
+// import { useNavigate } from "react-router-dom";
+// import { useAuth } from "../context/AuthContext";
+// import { loginRequest } from "../api/auth.api";
+
+// export const AuthPage = () => {
+//   const navigate = useNavigate();
+//   const { login } = useAuth();
+
+//   const [email, setEmail] = useState("");
+//   const [password, setPassword] = useState("");
+//   const [error, setError] = useState("");
+
+//   const handleLogin = async () => {
+//     try {
+//       setError("");
+
+//       if (!email || !password) {
+//         setError("Введите логин и пароль");
+//         return;
+//       }
+
+//       const res = await loginRequest( { email, password } );
+
+//       // accessToken приходит в body
+//       login(res.data.accessToken);
+
+//       navigate("/", { replace: true });
+//     } catch (err) {
+//       setError("Неверный логин или пароль");
+//     }
+//   };
+
+//   return (
+//     <Box
+//       display="flex"
+//       flexDirection="column"
+//       gap={2}
+//       maxWidth={360}
+//       margin="100px auto"
+//     >
+//       <Typography variant="h5" textAlign="center">
+//         Авторизация
+//       </Typography>
+
+//       <TextField
+//         label="Email"
+//         value={email}
+//         onChange={(e) => setEmail(e.target.value)}
+//         fullWidth
+//       />
+
+//       <TextField
+//         label="Пароль"
+//         type="password"
+//         value={password}
+//         onChange={(e) => setPassword(e.target.value)}
+//         fullWidth
+//       />
+
+//       {error && (
+//         <Typography color="error" fontSize={14}>
+//           {error}
+//         </Typography>
+//       )}
+
+//       <Button variant="contained" onClick={handleLogin}>
+//         Войти
+//       </Button>
+//     </Box>
+//   );
+// };
