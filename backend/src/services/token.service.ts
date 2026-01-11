2a7395cfc5fbfb839fa33b647c2e9fc034e9bef9
Denis Pyatirov <denis.pyatirov@gmail.com>
2026-01-11 21:04:46 +0300

    Для токенов

--- backend/src/services/token.service.ts
+++ backend/src/services/token.service.ts
@@ -1,14 +1,23 @@
-import jwt from "jsonwebtoken"
+import "dotenv/config"
+import jwt from 'jsonwebtoken';
 
-const ACCESS_SECRET = process.env.JWT_ACCESS_SECRET!
-const REFRESH_SECRET = process.env.JWT_REFRESH_SECRET!
+const ACCESS_SECRET = process.env.JWT_ACCESS_SECRET!;
+const REFRESH_SECRET = process.env.JWT_REFRESH_SECRET!;
 
-export function generateAccessToken(payload: {userId: string, role: string}) {
-    return jwt.sign(payload, ACCESS_SECRET, { expiresIn: '10m' })
+console.log('ACCESS_SECRET:', ACCESS_SECRET);
+console.log('REFRESH_SECRET:', REFRESH_SECRET);
+
+
+if (!ACCESS_SECRET || !REFRESH_SECRET) {
+  throw new Error('JWT secrets are not defined in .env');
 }
 
-export function generateRefreshToken(payload: {userId: string}) {
-    return jwt.sign(payload, REFRESH_SECRET, { expiresIn: "30d" })
+export function generateAccessToken(payload: { userId: string; role: string }) {
+  return jwt.sign(payload, ACCESS_SECRET, { expiresIn: '10m' });
+}
+
+export function generateRefreshToken(payload: { userId: string }) {
+  return jwt.sign(payload, REFRESH_SECRET, { expiresIn: '30d' });
 }
 
 export function verifyRefreshToken(token: string) {
@@ -18,4 +27,14 @@ export function verifyRefreshToken(token: string) {
     catch (err) {
         throw new Error("Invalid refresh token")
     }
+}
+
+export function verifyAccessToken(token: string) {
+  try {
+    // throws если токен невалиден или истёк
+    const payload = jwt.verify(token, ACCESS_SECRET) as { userId: string; role: string }
+    return payload
+  } catch (err) {
+    throw new Error('Invalid access token')
+  }
 }
