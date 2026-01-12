8f6f8f7612029327db74bcef91d7b386046853f5
Denis Pyatirov <denis.pyatirov@gmail.com>
2026-01-13 00:09:18 +0300

    Добавил логин в контект для отображения его на странице

--- frontend/src/context/AuthContext.tsx
+++ frontend/src/context/AuthContext.tsx
@@ -1,36 +1,59 @@
 import { createContext, useContext, useEffect, useState } from "react";
 
+type User = {
+  email: string;
+}
+
 type AuthContextType = {
   isAuth: boolean;
+  user: User | null;
   login: (accessToken: string) => void;
   logout: () => void;
 };
 
 const AuthContext = createContext<AuthContextType | null>(null);
 
+const parseJwt = (token: string): any => {
+  try {
+    return JSON.parse(atob(token.split(".")[1]));
+  } catch {
+    return null;
+  }
+};
+
 export const AuthProvider = ({ children }: { children: React.ReactNode }) => {
   const [isAuth, setIsAuth] = useState(false);
+  const [user, setUser] = useState<User|null>(null)
 
   const login = (accessToken: string) => {
     localStorage.setItem("accessToken", accessToken);
+
+    const payload = parseJwt(accessToken);
+    if (payload?.email) {
+      setUser({ email: payload.email });
+    }
     setIsAuth(true);
   };
 
   const logout = () => {
     localStorage.removeItem("accessToken");
     setIsAuth(false);
+    setUser(null);
   };
 
-  // 🔁 проверка при загрузке сайта
-  useEffect(() => {
+   useEffect(() => {
     const token = localStorage.getItem("accessToken");
-    if (token) {
+    if (!token) return;
+
+    const payload = parseJwt(token);
+    if (payload?.email) {
+      setUser({ email: payload.email });
       setIsAuth(true);
     }
   }, []);
 
   return (
-    <AuthContext.Provider value={{ isAuth, login, logout }}>
+    <AuthContext.Provider value={{ isAuth, user, login, logout }}>
       {children}
     </AuthContext.Provider>
   );
