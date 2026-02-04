1768efffdaa5ccf0f8115d55f45f14958fa3ea8b
Denis Pyatirov <denis.pyatirov@gmail.com>
2026-02-04 23:17:42 +0300

    Добавил проброс API через EntityPage, чтобы избавиться от fetch в верстке компонента

--- frontend/src/types/entities.ts
+++ frontend/src/types/entities.ts
@@ -3,7 +3,16 @@ export interface EntityConfig<T> {
   endpoint: string;
   columns: { field: keyof T; headerName: string }[];
   fields: EntityField<T>[];
+  api: EntityApi<T>;
   filters?: EntityFilter<T>[];
+}
+
+export interface EntityApi<T> {
+  getAll: () => Promise<T[]>;
+  getById: (id: string) => Promise<T>;
+  create: (data: FormData | T) => Promise<T>;
+  update: (id: string, data: FormData | T) => Promise<T>;
+  delete: (id: string) => Promise<void>;
 }
 
 export interface EntityField<T = any> {
