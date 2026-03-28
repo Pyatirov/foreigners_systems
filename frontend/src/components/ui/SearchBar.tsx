237b4c4875842139e39d287d9cfd52ad2029719c
Denis Pyatirov <denis.pyatirov@gmail.com>
2026-03-28 23:10:35 +0300

    Добавлены иконки поиска и очистки

--- frontend/src/components/ui/SearchBar.tsx
+++ frontend/src/components/ui/SearchBar.tsx
@@ -1,18 +1,40 @@
-import { TextField } from "@mui/material";
+import { TextField, InputAdornment, IconButton } from "@mui/material";
+import { Search, Clear } from "@mui/icons-material";
 
-interface Props {
+interface SearchBarProps {
   query: string;
   onChange: (value: string) => void;
 }
 
-export const SearchBar = ({ query, onChange }: Props) => {
+export const SearchBar = ({ query, onChange }: SearchBarProps) => {
   return (
     <TextField
       fullWidth
-      label="Поиск"
       value={query}
       onChange={(e) => onChange(e.target.value)}
       size="small"
+      slotProps={{
+        input: {
+          startAdornment: (                      // иконка поиска слева
+            <InputAdornment position="start">
+              <Search fontSize="small" />
+            </InputAdornment>
+          ),
+          endAdornment: (                        // кнопка очистки справа
+            <InputAdornment position="end">
+              {query && (                        // только если есть текст
+                <IconButton
+                  onClick={() => onChange("")}   // очищаем через onChange родителя
+                  edge="end"
+                  size="small"
+                >
+                  <Clear fontSize="small" />
+                </IconButton>
+              )}
+            </InputAdornment>
+          ),
+        },
+      }}
     />
   );
 };
